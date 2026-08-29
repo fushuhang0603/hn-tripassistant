adc01e3642f32d324377eaab862971193e89116c
fsh <640372469@qq.com>
2026-08-29 20:38:22 +0800

    学习graph

---
+++ springai-alibaba-graph/src/test/java/com/hanghang/springaialibabagraph/AlibabaGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghang.springaialibabagraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlibabaGraphApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
